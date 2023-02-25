ae1c3bef598580cc4169d038d762e20434c3f48b
Ilya Korotkov <i59korotkov@gmail.com>
2023-02-25 19:16:55 +0300

    Added WGAN models and train loop

---
+++ WGAN/models.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channels_img: int, features_d: int) -> None:
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(
+                channels_img,
+                features_d,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+            ),
+            nn.LeakyReLU(0.2),
+            self._block(features_d, features_d*2, 4, 2, 1),
+            self._block(features_d*2, features_d*4, 4, 2, 1),
+            self._block(features_d*4, features_d*8, 4, 2, 1),
+            nn.Conv2d(features_d*8, 1, 4, 2, 0),
+        )
+    
+    def _block(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int) -> nn.Module:
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim: int, channels_img: int, features_g: int) -> None:
+        super().__init__()
+        self.model = nn.Sequential(
+            self._block(z_dim, features_g*16, 4, 1, 0),
+            self._block(features_g*16, features_g*8, 4, 2, 1),
+            self._block(features_g*8, features_g*4, 4, 2, 1),
+            self._block(features_g*4, features_g*2, 4, 2, 1),
+            nn.ConvTranspose2d(features_g*2, channels_img, 4, 2, 1),
+            nn.Tanh(),
+        )
+    
+    def _block(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int) -> nn.Module:
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def init_weights(model: nn.Module):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+def test():
+    N, in_channels, H, W = 8, 3, 64, 64
+    z_dim = 100
+    x = torch.randn((N, in_channels, H, W))
+
+    discriminator = Discriminator(in_channels, 8)
+    init_weights(discriminator)
+    assert discriminator(x).shape == (N, 1, 1, 1)
+
+    generator = Generator(z_dim, in_channels, 8)
+    init_weights(generator)
+    z = torch.randn((N, z_dim, 1, 1))
+    assert generator(z).shape == (N, in_channels, H, W)
+    print('Tests passed')
+
+
+if __name__ == '__main__':
+    test()
---
+++ WGAN/train.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.auto import tqdm
+
+from models import Discriminator, Generator, init_weights
+
+
+# Hyperparameters
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+LEARNING_RATE = 5e-5
+BATCH_SIZE = 64
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+Z_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
+CRITIC_ITERATIONS = 5
+WEIGHT_CLIP = 0.01
+
+# Data
+transform = transforms.Compose([
+    transforms.Resize(IMAGE_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        [0.5 for _ in range(CHANNELS_IMG)],
+        [0.5 for _ in range(CHANNELS_IMG)],
+    ),
+])
+dataset = datasets.MNIST(root='dataset/', train=True, transform=transform, download=True)
+loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+fixed_noise = torch.randn((32, Z_DIM, 1, 1)).to(device)
+
+# Models
+generator = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+discriminator = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
+init_weights(generator)
+init_weights(discriminator)
+generator.train()
+discriminator.train()
+
+# Optimizer
+opt_gen = optim.RMSprop(generator.parameters(), lr=LEARNING_RATE)
+opt_disc = optim.RMSprop(discriminator.parameters(), lr=LEARNING_RATE)
+
+# Tensorboard
+writer_real = SummaryWriter('runs/real')
+writer_fake = SummaryWriter('runs/fake')
+step = 0
+
+# Training
+for epoch in range(NUM_EPOCHS):
+    for batch_idx, (real, _) in enumerate(tqdm(loader)):
+        # Get real data
+        real = real.to(device)
+
+        # Train discriminator
+        for _ in range(CRITIC_ITERATIONS):
+            # Generate fake data
+            noise = torch.randn((BATCH_SIZE, Z_DIM, 1, 1))
+            fake = generator(noise)
+
+            disc_pred_real = discriminator(real).reshape(-1)
+            disc_pred_fake = discriminator(fake).reshape(-1)
+            disc_loss = -(torch.mean(disc_pred_real) - torch.mean(disc_pred_fake))
+
+            discriminator.zero_grad()
+            disc_loss.backward(retain_graph=True)
+            opt_disc.step()
+
+            for p in discriminator.parameters():
+                p.data.clamp_(-WEIGHT_CLIP, WEIGHT_CLIP)
+
+        # Train generator
+        disc_pred_fake = discriminator(fake).reshape(-1)
+        gen_loss = -torch.mean(disc_pred_fake)
+
+        generator.zero_grad()
+        gen_loss.backward()
+        opt_gen.step()
+
+        # Tensorbaord
+        if batch_idx % 10 == 0:
+            print(
+                f'Epoch: [{epoch+1}/{NUM_EPOCHS}] \ '
+                f'Disc loss: {disc_loss:.4f}, Gen loss: {gen_loss:.4f}'
+            )
+
+            with torch.no_grad():
+                fake = generator(fixed_noise)
+
+                img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+                img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
+
+                writer_fake.add_image('MNIST generated images', img_grid_fake, global_step=step)
+                writer_real.add_image('MNIST real images', img_grid_real, global_step=step)
+
+                step += 1
